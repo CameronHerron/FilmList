22de6fd4a4205635f9e4c0d76169810345719004
Cameron Herron <cameron.herron@ihire.com>
2023-07-12 15:01:21 -0400

    updated function to search by title to include streamer info and image url. added styling to movie cards.

--- vue/src/components/HomePage.vue
+++ vue/src/components/HomePage.vue
@@ -1,17 +1,23 @@
 <template>
     <div>
         <h1 class="header">
-            Find on Streaming
+            Search by Title
         </h1>
 
         <form @submit.prevent="searchForMovie(searchTerm)">
             <input v-model="searchTerm">
             <button>Search</button>
         </form>
-
-        <ul>
-            <li v-for="movie in movies" :key="movie.tmdbId">{{ movie.title }}</li>
-        </ul>
+        <body>
+            <ul>
+                <li class="movie-card" v-for="movie in movies" :key="movie.tmdbId">
+                    <div>{{ movie.title }}</div>
+                    <img :src="movie.image">
+                    <!-- <p>{{ movie.overview }}</p> -->
+                </li>
+            </ul>
+        </body>
+        
         
     </div>
 </template>
@@ -23,16 +29,40 @@
     const movies = ref([]);
     let searchTerm = ref('');
 
+
+
     function searchForMovie(searchTerm){
         StreamingService.getMovieDetailsByName(searchTerm).then((response) => {
             let movieList = response.data.result;
             movies.value.length = 0;
             movieList.forEach(m => {
-                movies.value.push(m)
+                movies.value.push({
+                    title: m.title,
+                    tmdbId: m.tmdbId,
+                    streamers: Object.hasOwn(m.streamingInfo, 'us') ? Object.keys(m.streamingInfo.us) : null
+                    // getStreamerDetails(m.streamingInfo)
+                    ,
+                    image: m.posterURLs.original
+                });
             });
         })
         return movies;
  }
+
+//  function getStreamerDetails(json){
+//     let streamerDetails = [];
+//     if(Object.hasOwn(json, 'us')){
+//             Object.keys(json.us).forEach(streamer =>{
+//                 if(streamer.type == "free" || streamer.type == "subscription"){
+//                     streamerDetails.push({
+//                         streamerName: streamer,
+//                         type: streamer.type
+//                     })
+//                 }
+//             })
+//     }
+//     return streamerDetails;
+//  }
 
 
 
@@ -40,5 +70,29 @@
 </script>
 
 <style>
+    body{
+        display: flex;
+        justify-content: center;
+    }
+    ul{
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .movie-card{
+        list-style-type: none;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        border: 2px solid black;
+        border-radius: 10px;
+        height: 50vh;
+        width:50vh;
+        padding: 10px;
+        margin: 20px;
+    }
+    img{
+        max-height:60%
+    }
 
 </style>
